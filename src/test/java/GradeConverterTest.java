1e4b5318bd56122e7466055a63a09dedda0b1bf1
kiet-study <kietphung.study@gmail.com>
2025-06-11 16:18:15 +0700

    Update

--- src/test/java/GradeConverterTest.java
+++ src/test/java/GradeConverterTest.java
@@ -90,7 +90,7 @@ public class GradeConverterTest {
     @Test
     public void testConvert13() throws Exception {
         String expected = "C";
-        String actual = converter.convert(80);
+        String actual = converter.convert(74);
         assertEquals(expected,actual);
     }
 }
